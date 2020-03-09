fe01409e52760a5b45aef584afe85d5cd2bfd42b
ByJonathanB <61747478+ByJonathanB@users.noreply.github.com>
2020-03-09 11:36:31 -0500

    aprro
    aprro

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Wasson
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    /* ala vergo perro
+    // bro ya
+    */
+    public static void main(String[] args) {
+        // aloo
+    }
+    
+}
